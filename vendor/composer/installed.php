44dfa05a5ca5d00bad4be02f48ea2b9982cd9426
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-05-23 15:28:26 +0200

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'crabston/umami-analytics',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bc183f98aeb20f6fe1dbca6b9629f9aa2b96c842',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'crabston/umami-analytics' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bc183f98aeb20f6fe1dbca6b9629f9aa2b96c842',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
