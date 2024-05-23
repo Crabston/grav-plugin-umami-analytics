bbdaa4e4a91ecc4112cbfb868058a45e488c8fb4
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-05-23 16:38:47 +0200

    add functionality

--- umami-analytics.php
+++ umami-analytics.php
@@ -10,7 +10,15 @@ use Grav\Common\Plugin;
  */
 class UmamiAnalyticsPlugin extends Plugin
 {
-    /**
+
+	/**
+	 * @var string serverUrl
+	 * @var string websiteId
+	 */
+	protected $serverUrl;
+	protected $websiteId;
+
+	/**
      * @return array
      *
      * The getSubscribedEvents() gives the core a list of events
@@ -20,8 +28,7 @@ class UmamiAnalyticsPlugin extends Plugin
      *     callable (or function) as well as the priority. The
      *     higher the number the higher the priority.
      */
-    public static function getSubscribedEvents(): array
-    {
+    public static function getSubscribedEvents(): array {
         return [
             'onPluginsInitialized' => [
                 // Uncomment following line when plugin requires Grav < 1.7
@@ -36,24 +43,44 @@ class UmamiAnalyticsPlugin extends Plugin
      *
      * @return ClassLoader
      */
-    public function autoload(): ClassLoader
-    {
+    public function autoload(): ClassLoader {
         return require __DIR__ . '/vendor/autoload.php';
     }
 
     /**
      * Initialize the plugin
      */
-    public function onPluginsInitialized(): void
-    {
+    public function onPluginsInitialized(): void {
         // Don't proceed if we are in the admin plugin
         if ($this->isAdmin()) {
             return;
         }
 
-        // Enable the main events we are interested in
-        $this->enable([
-            // Put your main events here
-        ]);
+	    $this->serverUrl = trim($this->config->get('plugins.umami-analytics.server_url', ''));
+	    $this->websiteId = trim($this->config->get('plugins.umami-analytics.website_id', ''));
+
+	    // Don't proceed if there is no website ID
+	    if (empty($this->websiteId)) {
+		    $this->grav['debugger']->addMessage('Umami Analytics Plugin: No website ID configured!', 'error');
+		    return;
+	    }
+
+	    // Enable the main event we are interested in
+	    $this->enable([
+		    'onOutputGenerated' => ['onOutputGenerated', 0],
+	    ]);
     }
+
+	/**
+	 * The output has been processed by the Twig templating engine and is now just a string of HTML.
+	 */
+	public function onOutputGenerated(): void {
+		$code = implode(PHP_EOL, [
+			'<!-- Umami Analytics Script -->',
+			"<script defer src=\"{$this->serverUrl}/script.js\" data-website-id=\"{$this->websiteId}\"></script>",
+		]);
+
+		$content = preg_replace('/<head\s?\S*?(>)/si', "$0\n\n{$code}\n", $this->grav->output);
+		$this->grav->output = $content;
+	}
 }
