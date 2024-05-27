4763cb04656b53d51324a48d2ec833ada44cc558
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-05-27 16:53:20 +0200

    add host_url, auto_track and domains fields

--- umami-analytics.php
+++ umami-analytics.php
@@ -14,9 +14,15 @@ class UmamiAnalyticsPlugin extends Plugin
 	/**
 	 * @var string serverUrl
 	 * @var string websiteId
+	 * @var string hostUrl
+	 * @var string autoTrack
+	 * @var string domains
 	 */
 	protected $scriptSrc;
 	protected $websiteId;
+	protected $hostUrl;
+	protected $autoTrack;
+	protected $domains;
 
 	/**
      * @return array
@@ -58,6 +64,9 @@ class UmamiAnalyticsPlugin extends Plugin
 
 	    $this->scriptSrc = trim($this->config->get('plugins.umami-analytics.script_src', 'https://us.umami.is'));
 	    $this->websiteId = trim($this->config->get('plugins.umami-analytics.website_id', ''));
+	    $this->hostUrl = trim($this->config->get('plugins.umami-analytics.host_url', ''));
+	    $this->autoTrack = trim($this->config->get('plugins.umami-analytics.auto_track', ''));
+	    $this->domains = trim($this->config->get('plugins.umami-analytics.domains', ''));
 
 	    // Don't proceed if there is no website ID
 	    if (empty($this->websiteId)) {
@@ -75,9 +84,20 @@ class UmamiAnalyticsPlugin extends Plugin
 	 * The output has been processed by the Twig templating engine and is now just a string of HTML.
 	 */
 	public function onOutputGenerated(): void {
+		// Required parameters
+		$srcParam = "src=\"{$this->scriptSrc}/script.js\"";
+		$websiteIdParam = "data-website-id=\"{$this->websiteId}\"";
+
+		// Optional parameters
+		$hostUrlParam = $this->hostUrl ? "data-host-url=\"{$this->hostUrl}\"" : '';
+		// TODO: fix autoTrackParam:
+		// if autoTrack is empty in config file, it should not be added to the script tag, if it is true, it should be added with value true, if it is false, it should be added with value false
+		$autoTrackPram = $this->autoTrack ? "data-auto-track=\"{$this->autoTrack}\"" : '';
+		$domaisParam = $this->domains ? "data-domains=\"{$this->domains}\"" : '';
+
 		$code = implode(PHP_EOL, [
 			'<!-- Umami Analytics Script -->',
-			"<script defer src=\"{$this->scriptSrc}/script.js\" data-website-id=\"{$this->websiteId}\"></script>",
+			"<script defer ${srcParam} ${websiteIdParam} ${hostUrlParam} ${autoTrackPram} ${domaisParam}></script>",
 		]);
 
 		$content = preg_replace('/<head\s?\S*?(>)/si', "$0\n\n{$code}\n", $this->grav->output);
